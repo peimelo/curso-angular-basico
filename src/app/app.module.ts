2c9d8d58370801bdbe39387c2d6fb01a154d0686
Paulo Melo <peimelo@gmail.com>
2020-06-25 12:06:49 -0300

    v0.5.0: Edicao do Hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
