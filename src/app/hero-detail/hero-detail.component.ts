65581752d52eb96fd60d58b09523287c5835b6e8
Paulo Melo <peimelo@gmail.com>
2020-07-04 17:48:14 -0300

    v0.18.0: Update Hero

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -31,4 +31,8 @@ export class HeroDetailComponent implements OnInit {
   goBack() {
     this.location.back();
   }
+
+  save() {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
@@ -11,6 +11,10 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
   private heroesUrl = `${environment.baseUrl}/heroes`;
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
   constructor(
     private http: HttpClient,
@@ -30,6 +34,15 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(() => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>('getHeroes'))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http.put(url, hero, this.httpOptions).pipe(
+      tap(() => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<Hero>('updateHeroes'))
     );
   }
 
